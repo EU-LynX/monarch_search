2fbb76bdc6e290acf903b1d729d86e48411b0ca6
EU-LynX <79086006+EU-LynX@users.noreply.github.com>
2021-02-20 13:55:04 +0100

    Add files via upload

---
+++ monarch_search.py
@@ -0,0 +1,156 @@
+# py code to type in an year and than show the monarch at this time
+# v1.0
+
+print("Enter a year between 1000 & 2000): ")
+x = input()
+
+if x >= '1000' and x <= '1013':
+   print("Aethelred")
+
+if x >= '1013' and x <= '1014':
+   print("Sweyn")
+   
+if x >= '1014' and x <= '1016':
+   print("Aethelred the Unready")
+
+if x == '1016':
+   print("Edmund Ironside")
+   
+if x >= '1016' and x <= '1035':
+    print("Cnut the Great")
+    
+if x >= '1035' and x <= '1040':
+    print("Harold Harefoot")
+    
+if x >= '1040' and x <= '1042':
+    print("Harthacnut")
+    
+if x >= '1042' and x <= '1066':
+    print("Edward the Confessor")
+    
+if x == '1066':
+    print("Harold Godwinson")
+    print("Edgar Aetheling")    
+    
+if x >= '1066' and x <= '1087':
+    print("William I the Conquer")
+    
+if x >= '1087' and x <= '1100':
+    print("William II")
+    
+if x >= '1100' and x <= '1135':
+    print("Henry I")
+    
+if x >= '1135' and x <= '1154':
+    print("Stephen")
+    
+if x >= '1154' and x <= '1189':
+    print("Henry II")
+    
+if x >= '1189' and x <= '1199':
+    print("Richard I")
+    
+if x >= '1199' and x <= '1216':
+    print("John")
+    
+if x >= '1216' and x <= '1272':
+    print("Henry III")
+    
+if x >= '1272' and x <= '1307':
+    print("Edward I")
+    
+if x >= '1307' and x <= '1327':
+    print("Edward II")
+    
+if x >= '1327' and x <= '1377':
+    print("Edward III")
+    
+if x >= '1377' and x <= '1399':
+    print("Richard II")
+    
+if x >= '1399' and x <= '1413':
+    print("Henry IV")
+    
+if x >= '1413' and x <= '1422':
+    print("Henry V")
+    
+if x >= '1422' and x <= '1461':
+    print("Henry VI")
+    
+if x >= '1461' and x <= '1483':
+    print("Edward IV")
+
+if x == '1483':
+    print("Edward V")
+    
+if x >= '1483' and x <= '1485':
+    print("Richard III")
+    
+if x >= '1485' and x <= '1509':
+    print("Henry VII")
+    
+if x >= '1509' and x <= '1547':
+    print("Henry VIII")
+    
+if x >= '1547' and x <= '1553':
+    print("Edward VI")
+    
+if x == '1553':
+    print("Jane")
+    
+if x >= '1553' and x <= '1558':
+    print("Mary I")
+    
+if x >= '1558' and x <= '1603':
+    print("Elizabeth I")
+    
+if x >= '1603' and x <= '1625':
+    print("James I")
+    
+if x >= '1625' and x <= '1649':
+    print("Charles I")
+    
+if x >= '1649' and x <= '1659':
+    print("no monarch")
+    
+if x >= '1659' and x <= '1685':
+    print("Charles II")
+    
+if x >= '1685' and x <= '1688':
+    print("James II")
+    
+if x >= '1689' and x <= '1702':
+    print("William III")
+    
+if x >= '1702' and x <= '1714':
+    print("Anne")
+    
+if x >= '1714' and x <= '1727':
+    print("George I")
+    
+if x >= '1727' and x <= '1760':
+    print("George II")
+    
+if x >= '1760' and x <= '1820':
+    print("George III")
+    
+if x >= '1820' and x <= '1830':
+    print("George IV")
+    
+if x >= '1830' and x <= '1837':
+    print("William IV")
+    
+if x >= '1837' and x <= '1901':
+    print("Victoria")
+    
+if x >= '1901' and x <= '1910':
+    print("Edward VII")
+    
+if x >= '1910' and x <= '1936':
+    print("Edward VIII")
+    
+if x >= '1936' and x <= '1952':
+    print("George VI")
+    
+if x >= '1952':
+    print("Elisabeth II")    
